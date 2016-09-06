ee68ec63867ebfdf3133a680318524b8a62a0408
Victory <git@dfhu.org>
2016-09-06 14:32:07 -0500

    Initial commit

---
+++ app/src/main/java/org/dfhu/vpodplayer/Podcasts.java
@@ -0,0 +1,13 @@
+package org.dfhu.vpodplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Podcasts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_podcasts);
+    }
+}
