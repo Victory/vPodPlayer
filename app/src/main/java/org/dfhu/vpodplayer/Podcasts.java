6a4f02882197eeadbc1225e51ffa682bba68529c
Victory <git@dfhu.org>
2016-09-07 13:02:29 -0500

    Fetch XML for a feed of a hardcoded podcast

--- app/src/main/java/org/dfhu/vpodplayer/Podcasts.java
+++ app/src/main/java/org/dfhu/vpodplayer/Podcasts.java
@@ -1,13 +1,44 @@
 package org.dfhu.vpodplayer;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.dfhu.vpodplayer.feed.FetchFeed;
+import org.dfhu.vpodplayer.feed.FetchFeed.FeedResult;
 
 public class Podcasts extends AppCompatActivity {
+
+    Button mFetchFeeds;
+
+    private static final String sTestFeed = "http://www.npr.org/rss/podcast.php?id=510289";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podcasts);
+        mFetchFeeds = (Button) findViewById(R.id.testFetchFeed);
+
+        mFetchFeeds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FetchAFeed faf = new FetchAFeed();
+                faf.execute(sTestFeed);
+
+                Log.d("testing", "button was clicked 3");
+            }
+        });
+    }
+
+    private class FetchAFeed extends AsyncTask<String, Integer, FeedResult> {
+
+        @Override
+        protected FeedResult doInBackground(String... feedUrls) {
+            FeedResult result = FetchFeed.fetch(feedUrls[0]);
+            return result;
+        }
     }
 }
---
+++ app/src/main/java/org/dfhu/vpodplayer/feed/FetchFeed.java
@@ -0,0 +1,86 @@
+package org.dfhu.vpodplayer.feed;
+
+
+import android.util.Log;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static android.R.attr.data;
+
+public class FetchFeed {
+
+    public static class FeedResult {
+
+        enum Status {
+            MALFORMED_URL,
+            COULD_NOT_OPEN_URL,
+            COULD_NOT_READ_STREAM,
+            SUCCESS
+        }
+
+        public final String data;
+        public final Status status;
+        public final Exception exception;
+
+        /** Set the status, leave data empty and store the exception */
+        public FeedResult(Status status, IOException e) {
+            this.status = status;
+            this.exception = e;
+            this.data = "";
+            Log.w("Could not fetch feed", e);
+        }
+
+        /** Set the data. Status is SUCCESS and exception is null */
+        public FeedResult(String data) {
+            this.status = Status.SUCCESS;
+            this.data = data;
+            this.exception = null;
+        }
+    }
+
+    /**
+     * Gets the feed data. Assumes that the rss feed is encoded with UTF-8
+     *
+     * @param feedURL - full url with http(s) prefix
+     * @return has information about the attempted feed retrieval
+     */
+    public static FeedResult fetch(String feedURL) {
+        URL url;
+        try {
+            url = new URL(feedURL);
+        } catch (MalformedURLException e) {
+            return new FeedResult(FeedResult.Status.MALFORMED_URL, e);
+        }
+
+        HttpURLConnection connection;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            return new FeedResult(FeedResult.Status.COULD_NOT_OPEN_URL, e);
+        }
+
+        String data;
+        InputStream inputStream = null;
+        try {
+            inputStream = new BufferedInputStream(connection.getInputStream());
+            data = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            return new FeedResult(FeedResult.Status.COULD_NOT_READ_STREAM, e);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+
+        return new FeedResult(data);
+    }
+}
