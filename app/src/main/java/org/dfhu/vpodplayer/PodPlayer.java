2d66e486d1572336bd5146ed21b29e4c31f1c1fa
Victory <git@dfhu.org>
2016-10-04 16:53:19 -0500

    Add ExoPlayer library

---
+++ app/src/main/java/org/dfhu/vpodplayer/PodPlayer.java
@@ -0,0 +1,31 @@
+package org.dfhu.vpodplayer;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+
+public class PodPlayer {
+
+    public static SimpleExoPlayer play(Context context) {
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(mainHandler, videoTrackSelectionFactory);
+
+        LoadControl loadControl = new DefaultLoadControl();
+
+        return ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+    }
+}
