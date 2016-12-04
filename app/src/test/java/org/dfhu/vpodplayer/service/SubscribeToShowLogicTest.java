9a8f670ccaec45d8720a647d4401e309f670bd0e
Victory <git@dfhu.org>
2016-12-03 18:52:35 -0600

    Build SubsribeToShowLogic test

--- app/src/test/java/org/dfhu/vpodplayer/service/SubscribeToShowLogicTest.java
+++ app/src/test/java/org/dfhu/vpodplayer/service/SubscribeToShowLogicTest.java
@@ -2,10 +2,12 @@ package org.dfhu.vpodplayer.service;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.util.Log;
 
 import junit.framework.Assert;
 
 import org.dfhu.vpodplayer.feed.FeedFactory;
+import org.dfhu.vpodplayer.feed.SubscriptionManager;
 import org.dfhu.vpodplayer.sqlite.Episodes;
 import org.dfhu.vpodplayer.sqlite.Shows;
 import org.dfhu.vpodplayer.util.StringsProvider;
@@ -14,9 +16,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import rx.observers.TestSubscriber;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 
 @RunWith(PowerMockRunner.class)
@@ -53,9 +59,25 @@ public class SubscribeToShowLogicTest extends Assert {
     }
 
     @Test
+    @PrepareForTest(Log.class)
     public void testBuild() {
-        fail("TODO");
+        PowerMockito.mockStatic(Log.class);
+        mockFeedFactory = mock(FeedFactory.class);
 
+        SubscriptionManager subscriptionManager = new SubscriptionManager.Builder()
+                .showsDb(mockShowsDb)
+                .episodesDb(mockEpisodesDb)
+                .feedFactory(mockFeedFactory)
+                .build();
+
+        SubscribeToShowLogic subscribeToShowLogic = new SubscribeToShowLogic.Builder()
+                .showsDb(mockShowsDb)
+                .episodesDb(mockEpisodesDb)
+                .subscriptionManager(subscriptionManager)
+                .stringsProvider(stringsProvider)
+                .build();
+
+        subscribeToShowLogic.handleIntent();
     }
 
 }
