3297dc9d439e8905083e0336e27d2e4816ee1a55
Victory <git@dfhu.org>
2016-12-07 16:19:21 -0600

    cleanup

--- app/src/main/java/org/dfhu/vpodplayer/fragment/ShowListFragment.java
+++ app/src/main/java/org/dfhu/vpodplayer/fragment/ShowListFragment.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -89,7 +88,6 @@ public class ShowListFragment extends VicFragment {
 
         return true;
     }
-
 
     /**
      * Start the service that deletes listened episodes for this showId
