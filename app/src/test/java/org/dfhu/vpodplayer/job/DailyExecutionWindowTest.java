05e31f4908833b3eb092135a1a4b6318f9c671e2
Victory <git@dfhu.org>
2016-12-13 13:18:50 -0600

    Tests of different parts of the day for DailyExecutionWindow

--- app/src/test/java/org/dfhu/vpodplayer/job/DailyExecutionWindowTest.java
+++ app/src/test/java/org/dfhu/vpodplayer/job/DailyExecutionWindowTest.java
@@ -4,15 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DailyExecutionWindowTest extends Assert {
-    public int nowHour = 15;
-    public int nowMinute = 30;
+    private int nowHour = 15;
+    private int nowMinute = 30;
 
     @Test
     public void testLaterToday() {
         int targetHour = 16;
         int targetMinute = 45;
         int windowLengthInMinutes = 5;
-        long expectedStartMs = 1000L * (3600L * 1L + 15L * 60L);
+        long expectedStartMs = 1000L * (3600L + 15L * 60L);
         long expectedEndMs = expectedStartMs + windowLengthInMinutes * 60000L;
 
         DailyExecutionWindow e = new DailyExecutionWindow(
@@ -22,11 +22,11 @@ public class DailyExecutionWindowTest extends Assert {
     }
 
     @Test
-    public void earlierToday() {
-        int targetHour = 15;
-        int targetMinute = 30;
+    public void earlierInTheDay() {
+        int targetHour = 14;
+        int targetMinute = 15;
         int windowLengthInMinutes = 5;
-        long expectedStartMs = 1000L * (3600L * 23L);
+        long expectedStartMs = 1000L * (3600L * 23L + 45L * 60L);
         long expectedEndMs = expectedStartMs + windowLengthInMinutes * 60000;
 
         DailyExecutionWindow e = new DailyExecutionWindow(
@@ -36,18 +36,30 @@ public class DailyExecutionWindowTest extends Assert {
     }
 
     @Test
-    public void currentHour() {
+    public void laterInCurrentHour() {
+        int targetHour = 15;
+        int targetMinute = 45;
+        int windowLengthInMinutes = 5;
+        long expectedStartMs = 1000L * (15L * 60L);
+        long expectedEndMs = expectedStartMs + windowLengthInMinutes * 60000;
 
+        DailyExecutionWindow e = new DailyExecutionWindow(
+               nowHour, nowMinute, targetHour, targetMinute, windowLengthInMinutes);
+        assertEquals("startMs", expectedStartMs, e.startMs);
+        assertEquals("endMs", expectedEndMs, e.endMs);
     }
-
-    @Test
-    public void currentMinute() {
-
-    }
-
 
     @Test
     public void rightNow() {
+        int targetHour = 15;
+        int targetMinute = 30;
+        int windowLengthInMinutes = 5;
+        long expectedStartMs = 0;
+        long expectedEndMs = expectedStartMs + windowLengthInMinutes * 60000;
 
+        DailyExecutionWindow e = new DailyExecutionWindow(
+               nowHour, nowMinute, targetHour, targetMinute, windowLengthInMinutes);
+        assertEquals("startMs", expectedStartMs, e.startMs);
+        assertEquals("endMs", expectedEndMs, e.endMs);
     }
 }
